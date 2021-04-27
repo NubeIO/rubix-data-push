0098119d96eab3fa90c5a94bfbc0f2bd6532d7ca
zero88 <sontt246@gmail.com>
2021-04-27 09:22:49 +0700

    - Add mqtt setting and mqtt stream handler

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,6 @@
 from .app import create_app, db
 from .background import FlaskThread
 from .server import GunicornFlaskApplication
-from .setting import AppSetting
+from .setting import AppSetting, MqttSetting
 from .utils.color_formatter import ColorFormatter
 
